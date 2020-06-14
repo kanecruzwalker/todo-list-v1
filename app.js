f2db14f69ef50af747d01efc57b35a916036bc2f
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-13 23:28:33 -0700

    added logic to display today's current day and ejs templating to render information dynamically on list.ejs page, which is being summoned with res.render

--- app.js
+++ app.js
@@ -2,17 +2,41 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser")
 
-app.use("view engine", "ejs");
+app.set("view engine", "ejs");
 
 app.get("/" , function (req, res){
 
     var today = new Date();
     var currentDay = today.getDay();
+    var day = ""
+    var dayType = ""
+
+
+
+
     if(currentDay=== 6 || currentDay===0){
-        res.send("Yay it's the weekend!");
+        if(currentDay === 6){
+            day = "Saturaday"
+        }else{  
+            day = "Sunaday"
+        }
+        dayType = "Weekend"
     }else {
-        res.sendFile(__dirname + "/index.html"); 
+        if(currentDay === 1){
+            day= "Monday"
+        }else if (currentDay === 2){
+            day="Tuesday"
+        }else if (currentDay === 3){
+            day="Wednesday"
+        }else if (currentDay === 4){
+            day="Thursday"
+        }else if (currentDay === 5){
+            day="Friday"
+        }
+        dayType = "Weekday"
+
     }
+    res.render("list", {kindOfDay: dayType, todayDay: day });
 })
 
 app.listen(3000, function (){
