f6d8941006d2300562e6d525d5d0837b42edf96f
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-14 18:47:19 -0700

    implemented .toLocaleDateString(), with english and date options, and altered list.ejs , to exclude script in component areas.

--- app.js
+++ app.js
@@ -7,36 +7,14 @@ app.set("view engine", "ejs");
 app.get("/" , function (req, res){
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = ""
-    var dayType = ""
-
-
-
-
-    if(currentDay=== 6 || currentDay===0){
-        if(currentDay === 6){
-            day = "Saturaday"
-        }else{  
-            day = "Sunaday"
-        }
-        dayType = "Weekend"
-    }else {
-        if(currentDay === 1){
-            day= "Monday"
-        }else if (currentDay === 2){
-            day="Tuesday"
-        }else if (currentDay === 3){
-            day="Wednesday"
-        }else if (currentDay === 4){
-            day="Thursday"
-        }else if (currentDay === 5){
-            day="Friday"
-        }
-        dayType = "Weekday"
-
-    }
-    res.render("list", {kindOfDay: dayType, todayDay: day });
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options);
+    
+    res.render("list", {kindOfDay: day });
 })
 
 app.listen(3000, function (){
