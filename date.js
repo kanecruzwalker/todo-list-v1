768bdf214230515845ba02bb858c1a1c56fbbb6d
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-15 21:00:28 -0700

    turned let into const where appropriate

--- date.js
+++ date.js
@@ -1,22 +1,22 @@
 exports.getDate= function () {
 
-    let today = new Date();
-    let options = {
+    const today = new Date();
+    const options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
-    let day = today.toLocaleDateString("en-US", options);
+    const day = today.toLocaleDateString("en-US", options);
 
     return day;
 }
 exports.getDay= function () {
 
-    let today = new Date();
-    let options = {
+    const today = new Date();
+    const options = {
         weekday: "long",
     };
-    let day = today.toLocaleDateString("en-US", options);
+    const day = today.toLocaleDateString("en-US", options);
 
     return day;
 }
