f8edf56b19d318a278791bcd741a8dceeda04016
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-15 20:42:43 -0700

    added about page and implemented header and footer as components

--- app.js
+++ app.js
@@ -49,6 +49,10 @@ app.post("/work", function (req, res){
     res.redirect("/work");
 });
 
+app.get("/about", function (req, res){
+    res.render("about");
+})
+
 app.listen(3000, function (){
     console.log("You are connected to PORT 3000");
 });
